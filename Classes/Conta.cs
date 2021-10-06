3d658fd50fca2c33bf190ede7ce2ce52d42957f8
tatiavila <tatiplacebo@gmail.com>
2021-10-05 22:43:43 -0400

    TransferenciasBancarias

---
+++ Classes/Conta.cs
@@ -0,0 +1,67 @@
+﻿using Bank.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Classes
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta()
+        {
+        }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if(Saldo - valorSaque < (Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta: " + TipoConta + " | ";
+            retorno += "Nome: " + Nome + " | ";
+            retorno += "Saldo: " + Saldo + " | ";
+            retorno += "Credito: " + Credito + " | ";
+            return retorno;
+        }
+
+    }
+}
